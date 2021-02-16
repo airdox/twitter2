f15f2fd8f1920d6f67cc56e581eb14a7c8de01dc
airdox <kevcanico25@gmail.com>
2021-02-16 16:36:01 +0100

    ✨ init flux

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -13,8 +13,37 @@ class PostController extends AbstractController
      */
     public function index(): Response
     {
+
+        $tweetsAll = [
+            [
+                "name" => "nameTweet",
+                "date" => "Lundi 10 Janvier 2021"
+            ],
+            [
+                "name" => "nameTweet2",
+                "date" => "Mardi 11 Janvier 2021"
+            ],
+            [
+                "name" => "nameTweet3",
+                "date" => "Mercredi 12 Janvier 2021"
+            ]
+        ];
+
+        $tweetsFilter = [
+            [
+                "name" => "nameTweet",
+                "date" => "Lundi 10 Janvier 2021"
+            ],
+            [
+                "name" => "nameTweet2",
+                "date" => "Mardi 11 Janvier 2021"
+            ]
+        ];
+
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
+            'tweetsAll' => $tweetsAll,
+            'tweetsFilter' => $tweetsFilter
         ]);
     }
 }
